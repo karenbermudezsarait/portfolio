b81a4e2ea46323e49a34a9d54970bf4b569f912b
karen bermudez <karenbermudezmora@gmai.com>
2024-10-28 19:11:23 -0300

    ajustes mobtuar

--- js/scripts.js
+++ js/scripts.js
@@ -53,7 +53,8 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: '.lightbox',
+        scrollZoom: true,
     });
 
 });

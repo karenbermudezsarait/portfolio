0873a73401863a9a3570dec92c6015a81e41890b
karen bermudez <karenbermudezmora@gmai.com>
2024-11-07 22:50:31 -0300

    avances

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar 
     navbarShrink();
 
+    
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
